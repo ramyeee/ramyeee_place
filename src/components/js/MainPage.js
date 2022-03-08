978ae2e6ad57df25307a8c7a1d8f50f3b8362b48
suram kim <suramkay@gmail.com>
2022-03-08 15:51:04 +0900

    frame of Main Page First Version

--- src/components/js/MainPage.js
+++ src/components/js/MainPage.js
@@ -1,21 +1,34 @@
 import React from 'react';
-import backgroundImage from '../../img/paper_texture.jpg';
-import IDPic from '../../img/대표사진.jpg';
-import PhotoDeco from '../../img/MainPage/photo_deco.png';
-import ApplicationTitle from '../../img/MainPage/applicationform.png';
+import {useEffect, useState} from 'react';
+import woodMaterial from '../../img/wood_texture.jpg';
+import deco01 from '../../img/MainPage/deco/deco01.png';
+import deco02 from '../../img/MainPage/deco/deco02.png';
+
+import MainImage from '../../img/MainPage/main.png'
 import '../css/MainPage.css';
 
+
+const deco = [deco01, deco02];
+
 const MainpageTemplate = () => {
+    const [index, setIndex] = useState(0);
+
+    useEffect(() => {
+        const timeout = setTimeout(() => {
+            setIndex(prevIndex => (index == 1 ? 0 : prevIndex + 1));
+        }, 500);
+        return () => {
+            /* cleanup */
+            clearInterval(timeout);
+        };
+    /* on component render*/
+    }, [index]);
+
     return (
         <div>
-            <div>
-                <img className = "background-paper" src={backgroundImage}></img>
-                <img className = "title" src = {ApplicationTitle}></img>    
-            </div>
-            <div>               
-                <img className = "ID-pic" src={IDPic} width="300px"></img>
-                <img className = "ID-deco" src = {PhotoDeco}></img>
-            </div>
+            <img id = "desk" src = {woodMaterial}></img>
+            <img id = "background" src={MainImage}></img>
+            <img id = "deco" src = {deco[index]} />
         </div>
     )
 }
